adac006637aaa6d98bb77cfbb35d1f5ac7e3e1ce
Bruno Tarmann <bruno@tarmann.com.br>
2015-08-27 22:42:25 +0100

    fix path

--- install-sublime.sh
+++ install-sublime.sh
@@ -1,4 +1,3 @@
 # symlink user preference folders
-ln -s /Users/brunobarretto1/Dropbox/Preferences/Sublime3/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-ln -s /Users/brunobarretto1/Dropbox/Preferences/Sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
-
+ln -s ~/Dropbox/Preferences/Sublime3/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+ln -s ~/Dropbox/Preferences/Sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
